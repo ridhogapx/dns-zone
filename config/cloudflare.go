cf59f25aca6d9a944954b15e2091de20801bf9d9
RageNeko26 <rneko2006@gmail.com>
2024-01-08 13:37:42 +0700

    feat: Add types for handling Cloudflare config

--- config/cloudflare.go
+++ config/cloudflare.go
@@ -7,3 +7,8 @@ type Cloudflare = string
 var Token = os.Getenv("CLOUDFLARE_TOKEN")
 var Authorization Cloudflare = "Bearer " + Token
 var BaseURL Cloudflare = "https://api.cloudflare.com/client/v4"
+
+type CloudflareConfig struct {
+	Authorization Cloudflare
+	URL           Cloudflare
+}
