8f4d7ffd36bc9b130d476f5da789b22de72d72c9
RageNeko26 <rneko2006@gmail.com>
2024-01-08 13:47:39 +0700

    feat: Initialize Cloudflare config in entry point

--- main.go
+++ main.go
@@ -1,44 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
+	"dns-zone/cloudflare"
+	"dns-zone/config"
 )
 
-type Cloudflare = string
-
-var baseUrl Cloudflare = "https://api.cloudflare.com/client/v4"
-
 func main() {
-	verifyURL := baseUrl + "/user/tokens/verify"
-	req, err := http.NewRequest(http.MethodGet, verifyURL, nil)
-
-	tokenAuthorization := os.Getenv("CLOUDFLARE_TOKEN")
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", tokenAuthorization))
-
-	if err != nil {
-		fmt.Println("Failed to create instance of http request ", err)
-		return
+	initConfig := config.CloudflareConfig{
+		URL:           config.BaseURL,
+		Authorization: config.Authorization,
 	}
 
-	res, err := http.DefaultClient.Do(req)
-
-	if err != nil {
-		fmt.Println("Failed making http request ", err)
-		return
+	cloudflareAPI := cloudflare.RequestCloudflareImpl{
+		Config: initConfig,
 	}
 
-	body := res.Body
-	readBody, err := io.ReadAll(body)
+	cloudflareAPI.Verify()
 
-	if err != nil {
-		fmt.Println("Failed to read response body ", err)
-		return
-	}
-
-	fmt.Println("Result:", string(readBody))
 }
