0d420a0041b862e124adf3a22c63191566d159c0
RageNeko26 <rneko2006@gmail.com>
2024-01-09 10:17:09 +0700

    fix: Add global token configuration

--- config/cloudflare.go
+++ config/cloudflare.go
@@ -5,10 +5,12 @@ import "os"
 type Cloudflare = string
 
 var Token = os.Getenv("CLOUDFLARE_TOKEN")
+var GlobalKey = os.Getenv("GLOBAL_KEY")
 var Authorization Cloudflare = "Bearer " + Token
 var BaseURL Cloudflare = "https://api.cloudflare.com/client/v4"
 
 type CloudflareConfig struct {
 	Authorization Cloudflare
 	URL           Cloudflare
+	Token         Cloudflare
 }
